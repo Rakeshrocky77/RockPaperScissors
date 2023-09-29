169fc9fe1a4cfc5fe874f90dc6e94c7bb747ec1a
Rakesh <rakesh.h7997@gmail.com>
2023-09-29 14:01:20 +0530

    section update

--- Main.js
+++ Main.js
@@ -2,7 +2,35 @@ console.log("hai")
 const options = ["Rock","paper","scissors"];
 
 function getComputerChoice(){
-   console.log("Computer");
+   const choice = options[Math.floor(Math.random() * options.length)];
+   return choice;
+}
+
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "Tie";
+    } 
+    else if( 
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ){
+        return "Player"; 
+    }
+    else {
+        return "Computer";
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    const result = checkWinner(playerSelection, computerSelection);
+    if(result == "Tie"){
+        return "It's a Tie!"
+    }
+    else if (result == "Player"){
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
 }
 
 getComputerChoice();
+
